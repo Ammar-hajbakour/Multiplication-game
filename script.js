fd65c5502154ebc049e55a8e876113a0f3708534
$ <ammar.hajbakour@gmail.com>
2024-10-27 02:28:18 +0300

    third commit

--- script.js
+++ script.js
@@ -2,33 +2,45 @@ const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 const operation = document.getElementById('operation');
 const result = document.getElementById('result');
+const container = document.querySelector('.container')
 const playerInput = document.getElementById('playerInput');
 const inputForm = document.getElementById('inputForm');
 const correctSound = document.getElementById('correct');
 const worngSound = document.getElementById('wrong');
+const winerSound = document.getElementById('winerSound');
+const loserSound = document.getElementById('loserSound');
 const levelButtons = document.querySelectorAll('.level');
+const problemCount = document.getElementById('total');
+const correctCount = document.getElementById('true');
+const wrongCount = document.getElementById('false');
 const operations = ['+','-','&times;'];
 
+let pCount = 0,cCount=0,wCount=0;
+problemCount.textContent = pCount
+    correctCount.textContent = cCount
+    wrongCount.textContent = wCount
 window.onload = ()=>{
     playerInput.value = null;
     playerInput.setAttribute('disabled', true)
 }
 const randomNumber = (x)=>{
-    return Math.floor(Math.random() * x) 
+    return Math.round(Math.random() * x) 
 }
 
 let number1 = '...', number2,_operation = '&times;',_result,playerInputValue
 const reset = ()=>{
-    number2 = randomNumber(10),
+    number2 = randomNumber(10);
     num1.innerHTML = number1
     num2.innerHTML = number2
     operation.innerHTML = _operation
     result.innerHTML = '?'
-    result.style.borderColor='#778899'
+    result.style.borderColor='#555'
     playerInput.focus()
     playerInput.value = null
 }
 reset();
+
+
 const calculation = (n1,n2,o)=>{
     let result;
     switch(o){
@@ -46,22 +58,38 @@ const calculation = (n1,n2,o)=>{
     return result
 }
 
-
 inputForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     playerInputValue = playerInput.value
     _result = calculation(number1,number2,_operation)
     if(+playerInputValue === _result){
-        result.style.borderColor = 'green'
+        result.style.borderColor = 'rgb(0, 128, 79)'
         correctSound.play()
+        cCount++;
     }
     else{
-        result.style.borderColor = 'red'
+        result.style.borderColor = 'rgb(255, 0, 0)'
         worngSound.play()
+        wCount++
     } 
-
+    pCount++;
     result.innerHTML = _result
-
+    problemCount.textContent = pCount
+    correctCount.textContent = cCount
+    wrongCount.textContent = wCount
+    if(pCount >= 5){
+        const res = (cCount*100)/pCount
+        if( res >= 70){
+            winerSound.play() 
+        }
+        else {
+            loserSound.play()  
+        }
+        container.innerHTML = `<h1><span style="color:${res >= 70 ? 'rgb(0, 128, 79)' : 'rgb(255, 0, 0)'};">${res.toFixed(0)}</span> / 100</h1>`
+        playerInput.innerHTML = ''
+        setTimeout(()=> location.reload(), 5000)
+        return 
+    }
     setTimeout(() => {
         reset();
     }, 2000);
@@ -69,9 +97,22 @@ inputForm.addEventListener('submit', (e)=>{
 
 levelButtons.forEach((button)=>{
     button.addEventListener('click',()=>{
-        levelButtons.forEach(b=> b.classList.remove('apply'));
+        levelButtons.forEach(b=> {
+            b.classList.remove('apply')
+            b.style.animation = 'none'
+        });
         button.classList.add('apply');
-        number1 = Number(button.textContent)
+        const buttonContent = button.textContent
+        switch(buttonContent){
+            case '1-5': number1 = randomNumber(5)
+            break;
+            case '6-10':
+                const x = randomNumber(10)
+                number1 = x > 5 ? x : x + 5
+                break;
+            default: number1 = Number(buttonContent)
+            break;
+        }
         num1.innerHTML = number1
         playerInput.removeAttribute('disabled')
         reset();
