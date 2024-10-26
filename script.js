a2d46850e25e8601912546668b923035c2c60e62
$ <ammar.hajbakour@gmail.com>
2024-10-26 20:36:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const operation = document.getElementById('operation');
+const result = document.getElementById('result');
+const playerInput = document.getElementById('playerInput');
+const inputForm = document.getElementById('inputForm');
+const levelButtons = document.querySelectorAll('.level');
+const operations = ['+','-','&times;'];
+
+window.onload = ()=>{
+    playerInput.value = null;
+    playerInput.setAttribute('disabled', true)
+}
+const randomNumber = (x)=>{
+    return Math.floor(Math.random() * x) 
+}
+
+let number1 = '...', number2,_operation = '&times;',_result,playerInputValue
+const reset = ()=>{
+    number2 = randomNumber(10),
+    num1.innerHTML = number1
+    num2.innerHTML = number2
+    operation.innerHTML = _operation
+    result.innerHTML = '?'
+    result.style.borderColor='#778899'
+    playerInput.focus()
+    playerInput.value = null
+}
+reset();
+const calculation = (n1,n2,o)=>{
+    let result;
+    switch(o){
+        case operations[0]: result = n1 + n2
+        break;
+        case operations[1]: result = n1 - n2
+        break;
+        case operations[2]: result = n1 * n2
+        break;
+        case operations[3]: result = n1 / n2
+        break
+        default: result = null
+        break
+    }
+    return result
+}
+
+
+inputForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    playerInputValue = playerInput.value
+    _result = calculation(number1,number2,_operation)
+    if(+playerInputValue === _result){
+        result.style.borderColor = 'green'
+    }
+    else{
+        result.style.borderColor = 'red'
+    } 
+
+    result.innerHTML = _result
+
+    setTimeout(() => {
+        reset();
+    }, 2000);
+})
+
+levelButtons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        levelButtons.forEach(b=> b.classList.remove('apply'));
+        button.classList.add('apply');
+        number1 = Number(button.textContent)
+        num1.innerHTML = number1
+        playerInput.removeAttribute('disabled')
+        reset();
+    })
+})
+
